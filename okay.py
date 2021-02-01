445a64d7f12f4a0b7d68e4dfe0bb6e9dd61de8ce
uniqueusername123 <addison.invest@gmail.com>
2021-01-31 20:26:05 -0500

    more play code.
    Just playing around and trying more commits.

--- okay.py
+++ okay.py
@@ -1,2 +1,14 @@
 for i in range(5):
     print(i**2)
+
+#Alright awesome, let's add some more code to understand it better. 
+
+for i in range(20):
+    if i % 2 == 0:
+        print('git')
+    elif i % 5 == 0:
+        print('become stats master and get the understanding of markets')
+    else:
+        print('hub')
+
+#Alright let's see if this worked. 
